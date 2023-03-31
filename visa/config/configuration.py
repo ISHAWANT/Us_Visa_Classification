16b74966bb3cda8704b941fb14f67495a784ef02
ishawant kumar <ishawantkumar@gmail.com>
2023-03-31 16:32:13 +0530

    configuration done

--- visa/config/configuration.py
+++ visa/config/configuration.py
@@ -59,6 +59,24 @@ class Configuration:
             return data_ingestion_config
         except Exception as e:
             raise CustomException(e,sys) from e
+        
+    #DATA VALIDATION START
+    def get_data_validation_confgi(self)->DataValidationConfig:
+        try:
+            artifact_dir = self.training_pipeline_config.artifact_dir 
+            
+            data_validation_artifact_dir = os.path.join(artifact_dir,DATA_VALIDATION_ARTIFACT_DIR,self.time_stamp) 
+            
+            data_validation_config = self.config_info[DATA_VALIDATION_CONFIG_KEY] 
+            
+            schema_file_path = os.path.join(ROOT_DIR,data_validation_config[DATA_VALIDATION_SCHEMA_DIR_KEY],
+                                            data_validation_config[DATA_VALIDATION_SCHEMA_FILE_NAME_KEY]) 
+            data_validation_config = DataValidationConfig(schema_file_path=schema_file_path) 
+            return data_validation_config
+        
+        except Exception as e:
+            raise CustomException(e,sys) from e 
+        
     
     def get_training_pipeline_config(self) ->TrainingPipelineConfig:
         try:
@@ -72,4 +90,4 @@ class Configuration:
             logging.info(f"Training pipleine config: {training_pipeline_config}")
             return training_pipeline_config
         except Exception as e:
-            raise CustomException(e,sys) from e
+            raise CustomException(e,sys) from e 
--- visa/constant/__init__.py
+++ visa/constant/__init__.py
@@ -36,6 +36,10 @@ COLUMN_CASE_STATUS = "case_status"
 
 
 #Data Validation related variable 
+DATA_VALIDATION_ARTIFACT_DIR = 'data_validation' 
+DATA_VALIDATION_CONFIG_KEY = 'data_validation_config'
+DATA_VALIDATION_SCHEMA_FILE_NAME_KEY = 'schema_file_name'
+DATA_VALIDATION_SCHEMA_DIR_KEY = 'schema_dir'
 
 #Data Transformation variable 
 
