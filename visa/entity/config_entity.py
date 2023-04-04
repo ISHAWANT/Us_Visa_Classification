80ad87b3969515d34c3388f19d123ddae7739d80
ishawant kumar <ishawantkumar@gmail.com>
2023-04-04 11:39:22 +0530

    config_entity for data transformation

--- visa/entity/config_entity.py
+++ visa/entity/config_entity.py
@@ -9,4 +9,6 @@ TrainingPipelineConfig = namedtuple('TrainingPipelineConfig',['artifact_dir'])
 #Data Validation 
 DataValidationConfig = namedtuple("DataValidationConfig", ["schema_file_path"])
 
+# DATA TRANSFORMATION 
+DataTransformationConfig = namedtuple("DataTransformationConfig",["transformed_train_dir","transformed_test_dir","preprocessing_object_file_path"])
 
