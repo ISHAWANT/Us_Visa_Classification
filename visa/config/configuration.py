8292c4cd867efe67c5c7e730990d23db6f05a718
ishawant kumar <ishawantkumar@gmail.com>
2023-04-04 11:40:28 +0530

    configuration for data transformation

--- visa/config/configuration.py
+++ visa/config/configuration.py
@@ -85,6 +85,42 @@ class Configuration:
         except Exception as e:
             raise CustomException(e,sys) from e
         
+# DATA TRANSFORMATION  
+
+    def get_data_transformation_config(self)->DataTransformationConfig:
+        try:
+            artifact_dir = self.training_pipeline_config.artifact_dir
+            data_transformation_artifact_dir = os.path.join(
+                artifact_dir,DATA_TRANSFORMATION_ARTIFACT_DIR,self.time_stamp
+            )
+            data_transformation_config_info = self.config_info[DATA_TRANSFORMATION_CONFIG_KEY] 
+            preprocessed_object_file_path = os.path.join(
+                data_transformation_artifact_dir,
+                data_transformation_config_info[DATA_TRANSFORMATION_PREPROCESSING_DIR_KEY],
+                data_transformation_config_info[DATA_TRANSFORMATION_PREPROCESSED_FILE_NAME_KEY]
+            )
+            
+            transformed_train_dir = os.path.join(
+                data_transformation_artifact_dir,
+                data_transformation_config_info[DATA_TRANSFORMATION_DIR_NAME_KEY],
+                data_transformation_config_info[DATA_TRANSFORMATION_TRAIN_DIR_NAME_KEY]
+            )
+            transformed_test_dir = os.path.join(
+                data_transformation_artifact_dir,
+                data_transformation_config_info[DATA_TRANSFORMATION_DIR_NAME_KEY],
+                data_transformation_config_info[DATA_TRANSFORMATION_TEST_DIR_NAME_KEY]
+            )
+            
+            data_transformation_config=DataTransformationConfig(
+                preprocessing_object_file_path=preprocessed_object_file_path,
+                transformed_train_dir=transformed_train_dir,
+                transformed_test_dir=transformed_test_dir
+            )
+            logging.info(f"Data Transformation config:{data_transformation_config}")
+            return data_transformation_config
+        except Exception as e:
+            raise CustomException(e,sys) from e 
+        
     
     def get_training_pipeline_config(self) ->TrainingPipelineConfig:
         try:
