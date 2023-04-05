553923e4045d0fad8023c517d91c4bbc9e3b3957
ishawant kumar <ishawantkumar@gmail.com>
2023-04-05 18:24:11 +0530

    configuration done for tranier

---
+++ visa/components/model_trainer.py
@@ -0,0 +1,8 @@
+from visa.exception import CustomException 
+import sys 
+from visa.logger import logging
+from typing import List 
+from visa.entity.artifact_entity import DataTransformationArtifact,ModelTrainerArtifact
+from visa.entity.config_entity import ModelTrainerConfig 
+from visa.utils.utils import load_numpy_array_data,save_object,load_object 
+from visa.entity.
