ca6f0a2f52692304aae641d3ab5bfb950b743df8
ishawant kumar <ishawantkumar@gmail.com>
2023-04-07 14:14:02 +0530

    model trainer updated

--- visa/constant/__init__.py
+++ visa/constant/__init__.py
@@ -58,7 +58,7 @@ ONE_HOT_COLUMN_KEY= "Onehot_columns"
 ORDINAL_COLUMN_KEY ="Oridnal_columns"
 TRANSFORM_COLUMN_KEY= "Transformation_columns"
 
-#Model training variable 
+#Model training variable
 MODEL_TRAINER_ARTIFACT_DIR = 'model_trainer'
 MODEL_TRAINER_CONFIG_KEY = 'model_trainer_config'
 MODEL_TRAINER_TRAINED_MODEL_DIR_KEY = 'trained_model_dir'
@@ -68,4 +68,5 @@ MODEL_TRAINER_MODEL_CONFIG_DIR_KEY = 'model_config_dir'
 MODEL_TRAINER_MODEL_CONFIG_FILE_NAME_KEY = 'model_config_file_name'
 #Model Validation Variable 
 
-#Model Pusher Variable 
+#Model Pusher Variable
+
