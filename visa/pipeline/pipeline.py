ee1b155418309c2bebdc32c8b134fdaf8d956fde
ishawant kumar <ishawantkumar@gmail.com>
2023-04-21 11:43:13 +0530

    pipeline upadated for model evaluation and pusher

--- visa/pipeline/pipeline.py
+++ visa/pipeline/pipeline.py
@@ -1,26 +1,31 @@
-import sys 
-from visa.constant import * 
+from collections import namedtuple
+from datetime import datetime
+import uuid
+from visa.config.configuration import Configuartion
 from visa.logger import logging
 from visa.exception import CustomException
-from visa.entity.config_entity import * 
-from visa.utils.utils import read_yaml_file
-from visa.components.data_ingestion import DataIngestion
+from threading import Thread
+from typing import List
+
+from multiprocessing import Process
 from visa.entity.artifact_entity import DataIngestionArtifact
-from visa.config.configuration import Configuration
-#data Validation
+from visa.entity.artifact_entity import DataValidationArtifact, DataTransformationArtifact, ModelTrainerArtifact, ModelEvaluationArtifact, ModelPusherArtifact
 from visa.components.data_ingestion import DataIngestion
-# from visa.components.data_validation import DataValidationArtifact
-#data transformation
-# from visa.components.data_transformation import DataTransformationArtifact
-from visa.components.data_transformation import DataTransformation
 from visa.components.data_validation import DataValidation
-#Model trainer
-from visa.components.model_trainer import ModelTrainer 
-from visa.entity.artifact_entity import DataValidationArtifact,DataTransformationArtifact,ModelTrainerArtifact
+from visa.components.data_transformation import DataTransformation
+from visa.components.model_trainer import ModelTrainer
+from visa.components.model_evaluation import ModelEvaluation
+from visa.components.model_pusher import ModelPusher
+import os, sys
+from collections import namedtuple
+from datetime import datetime
+import pandas as pd
+
+
 
 class Pipeline():
 
-    def __init__(self, config: Configuration = Configuration()) -> None:
+    def __init__(self, config:  Configuartion= Configuartion()) -> None:
         try:
             self.config = config
         except Exception as e:
@@ -31,56 +36,78 @@ class Pipeline():
             data_ingestion = DataIngestion(data_ingestion_config=self.config.get_data_ingestion_config())
             return data_ingestion.initiate_data_ingestion()
         except Exception as e:
-            raise CustomException(e, sys) from e 
-        
-# Data Validation 
+            raise CustomException(e, sys) from e
 
-    def start_data_validation(self,data_ingestion_artifact:DataIngestionArtifact)->DataValidationArtifact:
+    def start_data_validation(self, data_ingestion_artifact:DataIngestionArtifact)-> DataValidationArtifact:
         try:
-            data_validation = DataValidation(data_validation_config=self.config.get_data_validation_config(),data_ingestion_artifact=data_ingestion_artifact)
+            data_validation = DataValidation(data_validation_config=self.config.get_data_validation_config(),
+                                             data_ingestion_artifact=data_ingestion_artifact)
             return data_validation.initiate_data_validation()
         except Exception as e:
-            raise CustomException(e,sys) from e 
-        
-    def satrt_data_transformation(self,data_ingestion_artifact:DataIngestionArtifact,
-                                  data_validation_artifact:DataValidationArtifact)->DataTransformationArtifact:
+            raise CustomException(e, sys) from e
+
+
+    def start_data_transformation(self,
+                                  data_ingestion_artifact: DataIngestionArtifact,
+                                  data_validation_artifact: DataValidationArtifact) -> DataTransformationArtifact:
         try:
-            data_transformation = DataTransformation(
+            data_transfromation = DataTransformation(
                 data_transformation_config=self.config.get_data_transformation_config(),
                 data_ingestion_artifact=data_ingestion_artifact,
                 data_validation_artifact=data_validation_artifact
             )
-            return data_transformation.initiate_data_transformation() 
+
+            return data_transfromation.initiate_data_transformation()
         except Exception as e:
-            raise CustomException(e,sys) from e 
-        
-    #Model trainer 
-    def start_model_trainer(self,data_transformation_artifact:DataTransformationArtifact)->ModelTrainerArtifact:
+            raise CustomException(e, sys) from e
+
+    def start_model_trainer(self, data_transformation_artifact: DataTransformationArtifact) -> ModelTrainerArtifact:
         try:
             model_trainer = ModelTrainer(model_trainer_config=self.config.get_model_trainer_config(),
-                                         data_transformation_artifact=data_transformation_artifact)
-            return model_trainer.initiate_model_trainer() 
+                                         data_transformation_artifact=data_transformation_artifact
+                                         )
+            return model_trainer.initiate_model_trainer()
         except Exception as e:
-            raise CustomException(e,sys) from e 
-        
-    
-    def run_pipeline(self):                                                   
+            raise CustomException(e, sys) from e
+
+    def start_model_evaluation(self, data_ingestion_artifact: DataIngestionArtifact,
+                               data_validation_artifact: DataValidationArtifact,
+                               model_trainer_artifact: ModelTrainerArtifact) -> ModelEvaluationArtifact:
         try:
-            data_ingestion_artifact = self.start_data_ingestion()
-            #data Validation 
-            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
-            #Data Transformation 
-            data_transformation_artifact = self.satrt_data_transformation(data_ingestion_artifact=data_ingestion_artifact,
-                                                                          data_validation_artifact=data_validation_artifact)
-            #Model trainer
-             
-            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+            model_eval = ModelEvaluation(
+                model_evaluation_config=self.config.get_model_evaluation_config(),
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_artifact=data_validation_artifact,
+                model_trainer_artifact=model_trainer_artifact)
+            return model_eval.initiate_model_evaluation()
         except Exception as e:
-            raise CustomException(e,sys) from e 
+            raise CustomException(e, sys) from e
 
+    def start_model_pusher(self, model_eval_artifact: ModelEvaluationArtifact) -> ModelPusherArtifact:
+        try:
+            model_pusher = ModelPusher(
+                model_pusher_config=self.config.get_model_pusher_config(),
+                model_evaluation_artifact=model_eval_artifact
+            )
+            return model_pusher.initiate_model_pusher()
+        except Exception as e:
+            raise CustomException(e, sys) from e
+        
+    def run_pipeline(self):
+        try:
+             #data ingestion
 
-        
-        
+            data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            data_transfromation_artifact = self.start_data_transformation(data_ingestion_artifact=data_ingestion_artifact,
+                                                                          data_validation_artifact=data_validation_artifact)
+            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transfromation_artifact)
+            model_evaluation_artifact = self.start_model_evaluation(data_ingestion_artifact=data_ingestion_artifact,
+                                                                    data_validation_artifact=data_validation_artifact,
+                                                                    model_trainer_artifact=model_trainer_artifact)
             
-            
+            model_pusher_artifact = self.start_model_pusher(model_eval_artifact=model_evaluation_artifact)
+
+        except Exception as e:
+            raise CustomException(e, sys) from e 
         
