e54be22bbe422ffbc0ac9ce48423fbaa104b258b
ishawant kumar <ishawantkumar@gmail.com>
2023-04-21 11:34:40 +0530

    config_entity updated

--- visa/entity/config_entity.py
+++ visa/entity/config_entity.py
@@ -1,23 +1,20 @@
 from collections import namedtuple
 
-DataIngestionConfig = namedtuple('DataIngestionConfig',['dataset_download_url','raw_data_dir','ingested_train_dir','ingested_test_dir'])
 
-# after that we need to define above variable in constant from config.yaml
+DataIngestionConfig=namedtuple("DataIngestionConfig",
+["dataset_download_url","raw_data_dir","ingested_train_dir","ingested_test_dir"])
 
-TrainingPipelineConfig = namedtuple('TrainingPipelineConfig',['artifact_dir']) 
 
-#Data Validation 
 DataValidationConfig = namedtuple("DataValidationConfig", ["schema_file_path"])
 
-# DATA TRANSFORMATION 
-DataTransformationConfig = namedtuple("DataTransformationConfig",["transformed_train_dir","transformed_test_dir","preprocessing_object_file_path"])
+DataTransformationConfig = namedtuple("DataTransformationConfig", ["transformed_train_dir",
+                                                                   "transformed_test_dir",
+                                                                   "preprocessed_object_file_path"])
 
-# Model Trainer 
-ModelTrainerConfig = namedtuple("ModelTrainerConfig",["trained_model_file_path","base_accuracy","model_config_file_path"])
+ModelTrainerConfig = namedtuple("ModelTrainerConfig", ["trained_model_file_path","base_accuracy", "model_config_file_path"])
 
-#Model Evaluation 
-ModelEvaluationConfig = namedtuple("ModelEvaluationConfig",["model_evaluation_file_path","time_stamp"]) 
+ModelEvaluationConfig = namedtuple("ModelEvaluationConfig", ["model_evaluation_file_path","time_stamp"])
 
-#Model Pusher 
-# ModelPusherConfig = namedtuple("ModelPusherConfig",["export_dir_path"])
+ModelPusherConfig = namedtuple("ModelPusherConfig", ["export_dir_path"])
 
+TrainingPipelineConfig = namedtuple("TrainingPipelineConfig", ["artifact_dir"])
