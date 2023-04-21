1f9c9873d2a657b332e7cdbaffde1fcda28ca4fb
ishawant kumar <ishawantkumar@gmail.com>
2023-04-21 11:35:23 +0530

    artifact_entity.py updated

--- visa/entity/artifact_entity.py
+++ visa/entity/artifact_entity.py
@@ -1,18 +1,23 @@
-from collections import namedtuple 
+from collections import namedtuple
 
-DataIngestionArtifact = namedtuple('DataIngestionArtifact',['train_file_path','test_file_path','is_ingested','message']) 
+DataIngestionArtifact = namedtuple("DataIngestionArtifact",
+[ "train_file_path", "test_file_path", "is_ingested", "message"])
 
-#For Data Validation
-DataValidationArtifact = namedtuple("DataValidationArtifact",["schema_file_path","is_validated","message"])
 
-#DATA TRANSFORMATION 
-DataTransformationArtifact = namedtuple("DataTransformationArtifact",['is_transformed','message','transformed_train_file_path',
-                                                                      'transformed_test_file_path','preprocessed_object_file_path'])
+DataValidationArtifact = namedtuple("DataValidationArtifact",
+["schema_file_path","is_validated","message"])
 
-#Model Traniner 
+DataTransformationArtifact = namedtuple("DataTransformationArtifact",
+ ["is_transformed", "message", "transformed_train_file_path","transformed_test_file_path",
+     "preprocessed_object_file_path"])
+
+
+
+
 ModelTrainerArtifact = namedtuple("ModelTrainerArtifact", ["is_trained", "message", "trained_model_file_path",
                                                            "train_f1", "test_f1", "train_accuracy", "test_accuracy",
-                                                           "model_accuracy"]) 
+                                                           "model_accuracy"])
 
-# Model Evaluation 
-ModelEvaluationArtifact = namedtuple("ModelEvaluationArtifact",["is_model_accepted","evaluated_model_path"]) 
+ModelEvaluationArtifact = namedtuple("ModelEvaluationArtifact", ["is_model_accepted", "evaluated_model_path"])
+
+ModelPusherArtifact = namedtuple("ModelPusherArtifact", ["is_model_pusher", "export_model_file_path"]) 
